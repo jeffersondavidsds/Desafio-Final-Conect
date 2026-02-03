3fce0543cca16f14246ce90d665139f49a0eb497
Jefferson David <jeffersondavidsd@gmail.com>
2026-02-03 19:37:40 -0300

    finalização de desktop do projeto, próxima etapa é a responsividade para mobile

--- src/componentes/Perfil/Perfil.jsx
+++ src/componentes/Perfil/Perfil.jsx
@@ -1,8 +1,38 @@
+import image from '../../assets/fotoperfil.png';
+import styles from '../Perfil/perfil.module.scss';
+
+
 function Perfil() {
   return (
-    <div>
-      <h1>Perfil de Usuario</h1>
+    
+    <section className={styles.container}>
+    <div className={styles.perfilContainer}>
+      <img src={image} alt="Foto de Perfil" />
+      <div className={styles.info}>
+    <div className={styles.perfil}>
+      <h1>Jefferson David</h1>
+      <h3>Voluntário Ativo</h3>
+      <p>Apaixonado por fazer a diferença na comunidade. Acredito que pequenas ações podem transformar vidas e estou sempre em busca de novas oportunidades para ajudar.</p>
     </div>
+    <div className={styles.local}>
+      <a>Rio de Janeiro</a>
+      <a>jeffersondavidsd@gmail.com</a>
+      <a>Membro desde 2026</a>
+    </div>
+    <div className={styles.atividades}>
+      <a>Educação</a>
+      <a>Meio Ambiente</a>
+      <a>Assistência Social</a>
+      <a>Design</a>
+    </div>
+    </div>
+    </div>
+
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+    </style>
+
+    </section>
   );
 }
 
--- src/componentes/QuemSomos/QuemSomos.jsx
+++ src/componentes/QuemSomos/QuemSomos.jsx
@@ -1,9 +1,34 @@
+import image from "../../assets/backgroundQuemSomos.png";
+import styles from "../../componentes/QuemSomos/quemsomos.module.scss";
+
+
 function QueSomos() {
   return (
-    <div>
-      <h1>Quem Somos</h1>
-      <p>Aqui você encontrará informações sobre nossa missão, visão e valores.</p>
-    </div>
+    <section className={styles.quemSomos}>
+      <img src={image} alt="Imagem Quem Somos" />
+      <section className={styles.textos}>
+        <div className={styles.titulo}>
+        <h1>Como a Connect atua em projetos sociais</h1>
+        </div>
+      <section className={styles.conteudo}>
+        <div>
+        <p>A Connect é uma iniciativa que conecta empresas, pessoas e organizações sociais com um propósito em comum: gerar impacto positivo e transformar realidades. Atuamos no desenvolvimento e apoio a projetos sociais nas áreas de educação, inclusão, tecnologia, esporte e assistência social, sempre com foco em impacto real e sustentável.</p>
+        </div>
+        <div>
+        <p>Há anos, trabalhamos aproximando quem quer ajudar de quem precisa, promovendo ações sociais estruturadas, transparentes e alinhadas às necessidades das comunidades. Acreditamos que a transformação acontece quando propósito, colaboração e responsabilidade caminham juntos.</p>
+        </div>
+        <div>
+        <p>Nosso compromisso é conectar boas causas a quem deseja fazer a diferença, criando pontes entre empresas, voluntários e projetos sociais que realmente transformam vidas.</p>
+        </div>
+      </section>
+
+      </section>
+
+      <style>
+      @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+      </style>
+      
+    </section>
   );
 }
 
